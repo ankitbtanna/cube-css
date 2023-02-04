994b03af2e09ae5e967a417e5c4c42b6c099fdc6
Ankit Tanna <atanna@slb.com>
2023-02-04 15:41:03 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cube-css-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cube-css';
+}
